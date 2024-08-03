6fb7694ea2517f3f52a298b9f1dc8842bcbcbc52
GeFernando <andresgivenchy@gmail.com>
2024-08-03 18:45:56 +0000

    homeApoderado

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -19,7 +19,21 @@ export const Home = () => {
             </nav>
             <div className="d-flex justify-content-center align-items-center vh-100">
                 <div className="text-center">
-                    <button type="button" onClick={() => navigate("/login")} className="boton-login btn ">
+                <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src="https://img.freepik.com/vector-gratis/kinder-dibujos-animados_33099-1136.jpg?t=st=1722670167~exp=1722673767~hmac=56e3eee026b9f7e9bc15fa4cdf453ccd3abf47d7a06d4c526039ccc0021605cf&w=826" className="imagen-a" alt="..." />
+                            </div>
+                            <div className="carousel-item">
+                                <img src="https://img.freepik.com/fotos-premium/tres-ninos-jugando-tablas-madera-pizarra-palabra-alfabeto-ella_763111-285296.jpg?w=740" className="imagen-b" alt="..." />
+                            </div>
+                            <div className="carousel-item">
+                                <img src="https://img.freepik.com/vector-gratis/ilustracion-jardin-infantes_1284-22416.jpg?t=st=1722671332~exp=1722674932~hmac=79783298245b2dd40ea247c23216a993a8cc42b2bd9ee6e770435293e615ab55&w=740" className="imagen-c" alt="..." />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container-fluid-a">
+                    <button type="button" onClick={() => navigate("/login")} className="boton-login btn">
                         <FontAwesomeIcon icon={faUser} className="me-2" />
                         Iniciar Sesión
                     </button>
@@ -27,10 +41,7 @@ export const Home = () => {
                         <FontAwesomeIcon icon={faUserPlus} className="me-2" />
                         Regístrate como Profesor
                     </button>
-                    {/* <button type="button" onClick={() => navigate("/register/guardian")} className="boton-registro btn">
-                        <FontAwesomeIcon icon={faUserPlus} className="me-2" />
-                        Regístrate como Apoderado
-                    </button> */}
+                    </div>
                 </div>
             </div>
         </div>
